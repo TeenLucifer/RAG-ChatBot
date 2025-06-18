3ac24636c3baeb6fe9d17282451c4672af1f85a0
TeenLucifer <841976402@qq.com>
2025-06-18 09:28:17 +0800

    segmentation用段落和chunk_size分割

--- segmentation.py
+++ segmentation.py
@@ -3,6 +3,9 @@ from llama_index.core.node_parser import MarkdownNodeParser
 from llama_index.readers.file import FlatReader
 from llama_index.core.node_parser import SentenceSplitter
 from pathlib import Path
+import re
+import copy
+from llama_index.core.schema import Document
 
 markdown_file_path = "./converted_docs/uav_swarm_page23-25/uav_swarm_page23-25.md"
 
@@ -17,14 +20,18 @@ nodes = parser.get_nodes_from_documents(documents)
 # 二次切分：中文按照段落进行切分, 公式特殊处理
 # 针对中文优化的段落切分（按句号/换行符分割，避免截断句子）
 text_splitter = SentenceSplitter(
-    chunk_size=300,  # 建议值：200-500字符
-    chunk_overlap=50,
-    separator="。\n",  # 优先按空行分段
-    paragraph_separator="。\n"  # 中文句号+换行作为段落边界
+    chunk_size=200,
+    chunk_overlap=10,
+    separator="。",
+    paragraph_separator="\r\n\r\n",
+    secondary_chunking_regex="。！？\n",
 )
 refined_nodes = text_splitter.get_nodes_from_documents(nodes)
 
+# TODO(wangjintao): 需要进一步处理公式/图标/代码块
+
 # 打印切分后的节点
 for node in refined_nodes:
+    #print(repr(node.get_content()))
     print(node.get_content())
     print("-" * 80)  # 分隔符
